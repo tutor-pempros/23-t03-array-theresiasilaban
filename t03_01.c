f4a6930fdd3e86fe2cc49fc437ac8929f288d877
theresiasilaban <143679782+theresiasilaban@users.noreply.github.com>
2024-02-06 21:04:30 +0700

    051

--- t03_01.c
+++ t03_01.c
@@ -4,8 +4,28 @@
 
 int main(int _argc, char **_argv)
 {
-
-  // your code here
-
+  int n, i, min, max;
+  
+  scanf("%d", &n);
+  
+  int angka[n];
+  
+  for (i = 0; i < n; i++){
+    scanf("%d", &angka[i]);
+  }
+  
+  min = angka[0];
+  max = angka[0];
+  
+  for (i = 1; i < n; i++){
+    if (angka[i] < min){
+      min = angka[i];
+    }
+    if (angka[i] > max){
+      max = angka[i];
+    }
+  }
+  printf("%d\n", min);
+  printf("%d\n", max);
   return 0;
 }
